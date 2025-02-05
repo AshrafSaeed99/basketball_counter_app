e330573c76d81743267022f93d07e958e1aabd72
Ashraf Saeed <dr.ashrafsaeed99@gmail.com>
2025-02-05 12:07:27 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BasketballCounter());
+}
+
+class BasketballCounter extends StatelessWidget {
+  const BasketballCounter({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
